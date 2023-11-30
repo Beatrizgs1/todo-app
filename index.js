acc51a84895ecd113762e7f6d902da3bbd7d25ee
Beatrizgs1 <goncalvesbeatriz633@gmail.com>
2023-11-30 00:31:16 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ app.set('view engine' , 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/criar', (requisicao, resposta) =>{
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+    INSERT INTO tarefas(descricao, completa)
+    VALUES('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql,(erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+        resposta.redirect('/')
+    })
+})
+
 app.get('/' , (requisicao, resposta) => {
     resposta.render('home')
 })
@@ -28,7 +51,7 @@ conexao.connect((erro) => {
         return console.log(erro)
     }
     console.log("estou conectado ao  o Mysql")
-    
+
     app.listen(3004, () => {
         console.log("Servidor rodando na porta 3003!")
     })
